c1209c495ec1cf1a98770e136e76953108701f92
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2024-04-02 15:30:27 +1100

    using fixtures

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { test as base, expect } from "@playwright/test";
+import { TodoPage } from "./todo";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+// Extend basic test by providing a "todoPage" fixture.
+const test = base.extend<{ todoPage: TodoPage }>({
+  todoPage: async ({ page }, use) => {
+    const todoPage = new TodoPage(page);
+    await todoPage.goto();
+    await todoPage.addToDo("item1");
+    await todoPage.addToDo("item2");
+    await use(todoPage);
+    await todoPage.removeAll();
+  },
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("adds todo", async ({ todoPage }) => {
+  // Expect a title "to contain" a substring.
+  await todoPage.addToDo("my item");
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
